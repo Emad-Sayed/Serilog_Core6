e720466e22ef02d111c1b39f642f8a1c17bc1263
emad sayed <emad_tttt52@hotmail.com>
2022-04-21 16:36:28 +0200

    init

---
+++ SerilogDemo/Program.cs
@@ -0,0 +1,38 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// Serilog Configuration
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build(); 
+     builder.Host.UseSerilog((ctx, lc) => lc
+        .ReadFrom.Configuration(configuration)
+    );
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
